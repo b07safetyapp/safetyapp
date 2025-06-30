d483c10961cac5396bea1fdc9f022e95e1323b20
digitalyoshixi <digitalyoshixi@proton.me>
2025-06-30 13:49:47 -0400

    boilerplate setup

---
+++ app/src/main/java/com/safetyapp/mainapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.safetyapp.mainapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        EditText editText1 = findViewById(R.id.editText1);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String text = editText1.getText().toString();
+
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
