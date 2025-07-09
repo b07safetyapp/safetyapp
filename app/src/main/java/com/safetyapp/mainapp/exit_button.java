c445e1eeeff4ab6c8c4dd61c23cffca4c40fc2ae
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-07-09 00:14:09 -0400

    refactor: extended exit button class

--- app/src/main/java/com/safetyapp/mainapp/exit_button.java
+++ app/src/main/java/com/safetyapp/mainapp/exit_button.java
@@ -1,36 +1,32 @@
 package com.safetyapp.mainapp;
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-public class exit_button extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.exit_button);
-
-        Button exitButton = findViewById(R.id.button2);
-
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri webpage = Uri.parse("https://www.youtube.com/");
-                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-
-                startActivity(webIntent);
-
-                finishAffinity();
-            }
-        });
-    }
-}
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.view.View;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.content.Intent;
+//import android.net.Uri;
+// button logic incase needed later
+//public class exit_button extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_base);
+//
+//        Button exitButton = findViewById(R.id.exitbtn);
+//
+//        exitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Uri webpage = Uri.parse("https://www.youtube.com/");
+//                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+//
+//                startActivity(webIntent);
+//
+//                finishAffinity();
+//                finishAndRemoveTask();
+//            }
+//        });
+//    }
+//}
