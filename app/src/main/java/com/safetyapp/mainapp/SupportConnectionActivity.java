55e63841e97c9fa0fe85dacdc7be3e8328b743d9
yoshixi <davidchenyuhe@proton.me>
2025-07-14 18:00:14 -0400

    feat: reading from the dynamic quesitonaire saved json to get the users location

--- app/src/main/java/com/safetyapp/mainapp/SupportConnectionActivity.java
+++ app/src/main/java/com/safetyapp/mainapp/SupportConnectionActivity.java
@@ -1,9 +1,16 @@
 package com.safetyapp.mainapp;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 
@@ -18,8 +25,24 @@ public class SupportConnectionActivity extends AppCompatActivity {
 
         // Temp, will change later
         String userCity = "Toronto";
-
-
+        // read the current city from the saved questionaire json
+        // open the file and parse into json
+        try{
+            File questionresultsjson = new File(this.getFilesDir(), "questionresults.json");
+            String outputresultjsoncontents = "";
+            Scanner fileScanner = new Scanner(questionresultsjson);
+            while (fileScanner.hasNext()){
+                outputresultjsoncontents += fileScanner.nextLine();
+            }
+            fileScanner.close();
+            // parse into a json file
+            JSONObject rootjson = new JSONObject(outputresultjsoncontents);
+            userCity = rootjson.getString("q0");
+        }
+        catch (Exception e){
+            Log.d("error", e.getMessage());
+        }
+        Log.d("USER CITY", userCity);
 
 
         SupportDataParser parser = new SupportDataParser(this);
