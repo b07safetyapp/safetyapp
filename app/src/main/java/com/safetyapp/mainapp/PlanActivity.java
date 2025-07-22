4e155f56904004b3904d0db3c1760a9b32e63812
yoshixi <davidchenyuhe@proton.me>
2025-07-22 19:59:25 -0400

    feat: adding the activity recyucler view for displaying the plans

--- app/src/main/java/com/safetyapp/mainapp/PlanActivity.java
+++ app/src/main/java/com/safetyapp/mainapp/PlanActivity.java
@@ -3,6 +3,7 @@ package com.safetyapp.mainapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PlanActivity extends AppCompatActivity {
@@ -11,11 +12,12 @@ public class PlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
-        setTitle("Planning");
         // check the login
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         // get the items
-        PlanAdapter adapter = new PlanAdapter();
+        PlanAdapter adapter = new PlanAdapter(this.getApplicationContext());
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setTitle("Planning");
     }
 }
