0f90070b1a8e450776cfd467f1abb0a72e8ce6b5
yoshixi <davidchenyuhe@proton.me>
2025-07-22 20:32:26 -0400

    chore: merging with previuous branches

--- app/src/main/java/com/safetyapp/mainapp/PlanActivity.java
+++ app/src/main/java/com/safetyapp/mainapp/PlanActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PlanActivity extends AppCompatActivity {
+public class PlanActivity extends BaseActivity {
 
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_plan;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plan);
         // check the login
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         // get the items
